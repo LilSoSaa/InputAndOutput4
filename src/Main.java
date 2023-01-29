4dc2d20f4c802ba2ae1425974692a45295d022bb
giova <giovannitopor@gmail.com>
2023-01-30 00:06:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String name = "Lorenzo";
+          double Pi;
+        Pi = 3.141590;
+        System.out.printf("%s",  name.toUpperCase() +"\n");
+        System.out.printf( "The  number is : %f . ",Pi);
+
+
+
+    }
+    }
